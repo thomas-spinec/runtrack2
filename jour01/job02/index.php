b8d4b0e47945ddea433bf64c22a53242d792550d
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-14 14:55:41 +0100

    réalisation du job 02 du jour 1

---
+++ jour01/job02/index.php
@@ -0,0 +1,29 @@
+<!-- Partie PHP -->
+<?php
+$str = 'LaPlateforme';
+$str2 = 'Hello';
+$str3 = '!';
+
+$val = 6;
+$myBool = false
+?>
+
+<!-- Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 1 job02</title>
+</head>
+<body>
+    <h1>jour 1 job02</h1>
+    <p><?php echo $str2 . ' ' . $str . ' ' . $str3; // Ceci est un commentaire sur une ligne ?></p>
+    <br>
+    <p><?php echo $str2 . ' ' . $str . ' ' . $str3; /* Ceci est un commentaire 
+    sur plusieurs lignes */?></p> 
+    
+</body>
+</html>
+
