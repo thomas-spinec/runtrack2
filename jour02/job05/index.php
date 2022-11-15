f6d977384d0ba542acfc5fb3d161234f65d1e7e5
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-15 11:59:15 +0100

    réalisation du job05 du jour 2

---
+++ jour02/job05/index.php
@@ -0,0 +1,38 @@
+<!-- Partie PHP -->
+<?php
+// inutile dans ce job
+?>
+
+<!-- Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 2 job05</title>
+</head>
+<body>
+    <h1>jour 2 job05</h1>
+    <br>
+    <p><?php 
+        for($i=1; $i<=1000; $i++){
+            if ($i < 3){ //affiche 1 et 2 car sont des nombres premiers
+                echo $i;
+                echo "<br>";
+            }
+            else{
+                for($x=2; $x<$i; $x++){
+                    if ($i % $x === 0){ // si le modulo de i par rapport à n'importe quel chiffre au dessous = 0 on passe au prochain x
+                        break;
+                    }
+                    else if($x === $i-1){ // si on est arrivé jusqu'à i-1 sans déclencher la conditions précédente, c'est un nombre premier
+                        echo $i;
+                        echo "<br>";
+                    }
+                }
+            }
+        }
+    ?></p>
+</body>
+</html>
