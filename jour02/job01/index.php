d3159840762c4afe66e7eafc440f12b3802f5e20
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-15 10:41:50 +0100

    création du jour02, et réalisation du job1

---
+++ jour02/job01/index.php
@@ -0,0 +1,31 @@
+<!-- Partie PHP -->
+<?php
+// inutile dans ce job
+?>
+
+<!-- Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 2 job01</title>
+</head>
+<body>
+    <h1>jour 2 job01</h1>
+    <br>
+    <p><?php 
+        for($i=1; $i<1337; $i++){
+            if($i === 42){
+                echo "<b><u>$i</u></b>";
+                echo "<br>";
+            }
+            else{
+                echo $i;
+                echo "<br>";
+            }
+        }
+    ?></p>
+</body>
+</html>
