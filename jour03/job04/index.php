30aed197f733eb9e255296b0a2e8a2b3956de5b2
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-16 12:05:48 +0100

    réalisation du job 05 du jour 3 et modification du titre du job 04 (affichage)

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -11,10 +11,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>jour 3 job03</title>
+    <title>jour 3 job04</title>
 </head>
 <body>
-    <h1>jour 3 job03</h1>
+    <h1>jour 3 job04</h1>
     <br>
     <p><?php 
         echo "<b>$str</b><br><br>";
---
+++ jour03/job05/index.php
@@ -0,0 +1,51 @@
+<!-- Partie PHP -->
+<?php
+    $str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
+    $voy = ["a", "e", "i", "o", "u", "y"];
+    $maj = ["A", "E", "I", "O", "U", "Y"];
+    $nb_voy = 0;
+    $nb_cons = 0;
+    $dic = [
+        "voyelles" => 0,
+        "consonnes" => 0
+    ];
+?>
+
+<!-- Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 3 job05</title>
+</head>
+<body>
+    <h1>jour 3 job05</h1>
+    <br>
+    <p><?php 
+        echo "<b>$str</b><br><br>";
+        for($i=0; isset($str[$i]); $i++){
+            for($j=0; $j<6; $j++){
+                if($str[$i] === $voy[$j] OR $str[$i] === $maj[$j]){  // cherche dans les voyelles en majuscule et en minuscule
+                    $dic["voyelles"]++;
+                    break;
+                }
+                else if($j==5 AND $str[$i]!=" " AND $str[$i]!="'" AND $str[$i]!="." AND $str[$i]!=","){
+                    $dic["consonnes"]++;
+                }
+            }
+        }
+    ?></p>
+    <table> <!-- Affichage du Tableau -->
+        <tr>
+            <th>Consonnes</th>
+            <th>Voyelles</th>
+        </tr>
+        <tr>
+            <td><?php echo $dic["consonnes"] ?></td>
+            <td><?php echo $dic["voyelles"] ?></td>
+        </tr>
+    </table>
+</body>
+</html>
