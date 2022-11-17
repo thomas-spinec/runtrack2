5dd0025a6f7fb1d013fe56badb18b8ff934890e2
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-17 09:18:04 +0100

    création du dossier 'jour04'

---
+++ jour04/job01/index.php
@@ -0,0 +1,22 @@
+<!-- Partie PHP -->
+<?php
+
+?>
+
+<!-- Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 4 job01</title>
+</head>
+<body>
+    <h1>jour 4 job01</h1>
+    <br>
+    <p><?php 
+
+    ?></p>
+</body>
+</html>
