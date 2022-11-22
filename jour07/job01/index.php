e054bbbf2103f0c1a29b97a51b48906ce8ea60b2
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-22 10:16:57 +0100

    création du dossier jour 07, réalisation du job1

---
+++ jour07/job01/index.php
@@ -0,0 +1,28 @@
+<!-- Partie PHP -->
+<?php
+    function hello(){
+        echo "Hello LaPlateforme!";
+    }
+?>
+
+<!-- Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 7 job01</title>
+</head>
+<body>
+    <h1>Les fonctions</h1>
+    <br>
+    <h1>jour 7 job01</h1>
+    <br>
+    <br>
+    <?php
+        hello();
+    ?>
+
+</body>
+</html>
