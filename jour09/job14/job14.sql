bf35ac4ec4c867b8879a2e4e888feb0861c1632f
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-24 14:32:29 +0100

    correction de la date du job 14

--- jour09/job14/job14.sql
+++ jour09/job14/job14.sql
@@ -1,3 +1,3 @@
 /* sélectionner le prenom, le
 nom et la date de naissance des étudiants qui sont nés entre 1998 et 2018. */
-SELECT `prenom`, `nom`, `naissance` FROM `etudiants` WHERE `naissance` BETWEEN '1998-01-01' AND '2018-01-01'
+SELECT `prenom`, `nom`, `naissance` FROM `etudiants` WHERE `naissance` BETWEEN '1998-01-01' AND '2018-12-31'
