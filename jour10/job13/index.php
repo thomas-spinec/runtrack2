f9a875f78c4d1e7cd1591bce71090ca6e1254212
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-25 12:11:19 +0100

    réalisation du job13 du jour 10

---
+++ jour10/job13/index.php
@@ -0,0 +1,65 @@
+<!--  Partie PHP -->
+<?php
+    // connexion à la base de donnée
+    $mysqli = new mysqli('localhost', 'root', '', 'jour09');
+
+    // requête
+    $request = $mysqli -> query("select `salles`.`nom`, `etage`.`nom` from salles inner join `etage` on `salles`.`id_etage` = `etage`.`id`");
+
+    // affichage
+
+    // $result_all = $request -> fetch_all();
+    // var_dump($result_all);
+
+    // while(($result_array = $request -> fetch_array()) != null){
+    //     var_dump($result_array);
+    // }
+
+?>
+
+<!--  Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table,
+        th,
+        td {
+            padding: 10px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+    <title>job13 jour 10</title>
+</head>
+
+<body>
+    <h1>SQL + PHP</h1>
+    <h3>job13 jour 10</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nom de la salle</th>
+                <th>Nom de l'étage</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+                <?php
+                while(($result = $request -> fetch_array()) != null)
+                {
+                    echo "<tr>";
+                    echo "<td>".$result[0]."</td>";
+                    echo "<td>".$result[1]."</td>";
+                    echo "</tr>";
+                }
+            ?> 
+            
+        </tbody>
+    </table>
+</body>
+</html>
