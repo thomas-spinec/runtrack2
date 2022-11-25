e88737b5ab1839a456b6996b4b1e9577fdc518a3
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-25 11:35:27 +0100

    réalisation du job07 du jour 10

---
+++ jour10/job07/index.php
@@ -0,0 +1,66 @@
+<!--  Partie PHP -->
+<?php
+    // connexion à la base de donnée
+    $mysqli = new mysqli('localhost', 'root', '', 'jour09');
+
+    // requête
+    $request = $mysqli -> query("select sum(`superficie`) from etage");
+
+    // affichage
+
+    // $result_all = $request -> fetch_all();
+    // var_dump($result_all);
+
+    // while(($result_array = $request -> fetch_array()) != null){
+    //     var_dump($result_array);
+    //     echo "<br>";
+    //     echo $result_array['count(*)'];
+    // }
+
+
+?>
+
+<!--  Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table,
+        th,
+        td {
+            padding: 10px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+    <title>job07 jour 10</title>
+</head>
+
+<body>
+    <h1>SQL + PHP</h1>
+    <h3>job07 jour 10</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Superficie_totale</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+                <?php
+                while(($result = $request -> fetch_array()) != null)
+                {
+                    echo "<tr>";
+                    echo "<td>".$result['sum(`superficie`)']."</td>";
+                    echo "</tr>";
+                }
+            ?> 
+            
+        </tbody>
+    </table>
+</body>
+</html>
