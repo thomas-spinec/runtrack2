742138ea4f69e70cb3903c6778cb7b5e12e4929a
thomas-spinec <thomas.spinec@laplateforme.io>
2022-11-25 10:43:28 +0100

    création du dossier jour 10, réalisation du job01 de ce jour

---
+++ jour10/job01/index.php
@@ -0,0 +1,72 @@
+<!--  Partie PHP -->
+<?php
+    // connexion à la base de donnée
+    $mysqli = new mysqli('localhost', 'root', '', 'jour09');
+
+    // requête
+    $request = $mysqli -> query("select * from etudiants");
+
+    // affichage
+
+    // $result_all = $request -> fetch_all();
+    // var_dump($result_all);
+
+    // while(($result_array = $request -> fetch_array()) != null){
+    //     var_dump($result_array);
+    //     echo "<br>";
+    //     echo $result_array[1];
+
+    // }
+
+
+?>
+
+<!--  Partie HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job01 jour 10</title>
+        <style>
+        table,
+        th,
+        td {
+            padding: 10px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        </style>
+</head>
+
+<body>
+    <h1>SQL + PHP</h1>
+    <h3>job01 jour 10</h3>
+
+    <table border : solid>
+        <thead>
+            <tr>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Naissance</th>
+                <th>Sexe</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                while(($result = $request -> fetch_array()) != null)
+                {
+                    echo "<tr>";
+                    echo "<td>".$result['prenom']."</td>";
+                    echo "<td>".$result['nom']."</td>";
+                    echo "<td>".$result['naissance']."</td>";
+                    echo "<td>".$result['sexe']."</td>";
+                    echo "<td>".$result['email']."</td>";
+                    echo "</tr>";
+                }
+            ?>
+    </table>
+</body>
+</html>
